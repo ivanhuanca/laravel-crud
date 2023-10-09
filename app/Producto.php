27526817e57092996c7f976b83c4c190d743d66c
Ivan Huanca <81381865+ivanhuanca@users.noreply.github.com>
2023-10-08 20:22:20 -0500

    Crud clientes, pedidos y productos agregados

---
+++ app/Producto.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Producto
+ *
+ * @property $id
+ * @property $name
+ * @property $stock
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Producto extends Model
+{
+    
+    static $rules = [
+		'name' => 'required',
+		'stock' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name','stock'];
+
+
+
+}
